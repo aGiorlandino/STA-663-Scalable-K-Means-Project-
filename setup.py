0c2d2be32ac9c503d71548b715a6f89d96cda2d9
Siddartha Gade <pgade8@gatech.edu>
2017-05-01 02:13:16 -0400

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Statistical_Computation',
-    version='0.1',
+    version='1.0',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Implementation of scalable k-means++',
