544ca129f7ce0117cdd7e8f4ae6efa6ae214b522
Siddartha Gade <pgade8@gatech.edu>
2017-05-01 01:52:57 -0400

    new setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Implementation of scalable k-means++',
-    long_description=open('README.md').read(),
     install_requires=['numpy'],
     url='https://github.com/susancherry/Statistical_Computation.git',
     author='Susan Cherry',
